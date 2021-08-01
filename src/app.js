05faf812dda2c3b64a45d95287fbf1511f4d8c19
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-08-01 23:15:07 +0530

    POST request complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+require('./db/connection')
+const Student = require('./models/students')
 const app = express();
 const port = process.env.PORT || 3001
 
+app.use(express.json());
+
 app.post('/students', (req,res) => {
-//   console.info('body', req.body)
-//   res.json(req.body);
-    res.send("Hello from Yash")
+    console.log(req.body);
+    const user = new Student(req.body)
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((err)=>{
+        res.status(400).send(err);
+    })
+    // res.send("Hello from Yash")
 })
 
 app.listen(port, ()=>{
