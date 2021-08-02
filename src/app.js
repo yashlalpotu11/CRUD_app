ca84b99ab650bdd734dfa5af602e3cce1d3aebc5
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-08-02 19:28:37 +0530

    GET request done

--- src/app.js
+++ src/app.js
@@ -6,16 +6,50 @@ const port = process.env.PORT || 3001
 
 app.use(express.json());
 
-app.post('/students', (req,res) => {
+app.post('/students',async(req, res)=>{
     console.log(req.body);
-    const user = new Student(req.body)
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((err)=>{
-        res.status(400).send(err);
-    })
-    // res.send("Hello from Yash")
+    try{
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser)
+    }catch(e){
+        res.status(400).send(e);
+    }
 })
+
+//read the data
+app.get('/students', async(req, res) => {
+    try{
+        const studentsData = await Student.find();
+        res.send(studentsData)
+    }catch(e){
+        res.send(e);
+    }
+})
+//get data of individual data
+app.get('/students/:id', async(req, res) => {
+    try{
+        const _id = req.params.id;
+        // console.log(_id);
+        
+        const studentData = await Student.findById({_id})
+        res.send(studentData)
+
+    }catch(e){
+        res.send(e);
+    }
+})
+
+
+// app.post('/students', (req,res) => {
+//     console.log(req.body);
+//     const user = new Student(req.body)
+//     user.save().then(() => {
+//         res.status(201).send(user)
+//     }).catch((err)=>{
+//         res.status(400).send(err);
+//     })
+// })
 
 app.listen(port, ()=>{
     console.log(`Connection is live on port ${port}`);
